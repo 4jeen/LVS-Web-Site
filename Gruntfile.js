a4d0814b7a84e2e862f3e0fd032fc1da4a999460
jeen <jeen@live.ru>
2016-08-05 01:33:43 +0300

    04/08/2016 first test virsion

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,67 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    imagemin: {
+      dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'src/HTML/assets/images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'dist/HTML/assets/images/'
+        }]
+      }
+    },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      basic_css: {
+        files: [{
+          expand: true,
+          cwd: 'src/HTML/assets/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/HTML/assets/css',
+          ext: '.css'
+        }]
+      },
+      color_scheme: {
+        files: [{
+          expand: true,
+          cwd: 'src/HTML/assets/css/color_scheme',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/HTML/assets/css/color_scheme',
+          ext: '.css'
+        }]
+      }
+    },
+
+
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options        
+          removeComments: true,
+          ignoreCustomComments: [ /^#include/ ],
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'dist/HTML/index.shtml.ru.utf8': 'src/HTML/index.shtml.ru.utf8',     // 'destination': 'source'
+          'dist/HTML/page-contact-3.shtml.ru.utf8': 'src/HTML/page-contact-3.shtml.ru.utf8',
+          'dist/HTML/footer.inc': 'src/HTML/footer.inc',     // 'destination': 'source'
+          'dist/HTML/shopcart.inc': 'src/HTML/shopcart.inc',
+          'dist/HTML/slidepanel.inc': 'src/HTML/slidepanel.inc',
+          'dist/HTML/slidetop.inc': 'src/HTML/slidetop.inc'
+        }
+      }
+    },
     copy: {
-	main: {
+	   fonts: {
 	    expand: true,
-	    cwd: 'src',
+	    cwd: 'src/HTML/assets/fonts',
 	    src: '*',
-	    dest: 'dist/',
-	},
+	    dest: 'dist/HTML/assets/fonts',
+	   },
     },
     uglify: {
       options: {
@@ -25,11 +79,17 @@ module.exports = function(grunt) {
   });
 
   // Load the plugin that provides the "uglify" task.
+    
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
 
   // Default task(s).
 //  grunt.registerTask('default', ['uglify']);
-    grunt.registerTask('default', ['copy']);
+    //grunt.registerTask('default', ['copy']);
+    grunt.registerTask('default', ['imagemin','htmlmin','cssmin','copy']);
 };
